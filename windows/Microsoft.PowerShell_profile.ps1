799978d3aa3616b03de294d767c66ff8b3af09a9
Krasimir Tsonev <krasimir@outset.ws>
2013-05-29 10:38:31 +0300

    Adding aliases for windows

---
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+# How to manage the profile file -> http://technet.microsoft.com/en-us/library/ee176913.aspx
+# Create 'WindowsPowerShell' in C:\Users\<username>\Documents\ and put the file there
+
+function goToProjects {set-location d:\work}
+
+Set-Alias projects goToProjects
