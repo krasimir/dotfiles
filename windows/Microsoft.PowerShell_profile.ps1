da101a48e07582070b623847dae1cd7286d736e3
Krasimir Tsonev <krasimir@outset.ws>
2013-05-29 10:46:43 +0300

    Adding git shortcuts[0]

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,14 @@
 # How to manage the profile file -> http://technet.microsoft.com/en-us/library/ee176913.aspx
 # Create 'WindowsPowerShell' in C:\Users\<username>\Documents\ and put the file there
 
-function goToProjects {set-location d:\work}
+function goToProjects {
+	set-location d:\work
+}
+function s {
+	git status
+}
+function ci {
+	git commit -am "$args[0]"
+}
 
 Set-Alias projects goToProjects
