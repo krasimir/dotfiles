51134b2139c105b876c0ee8c41bdc3323bc9774d
Krasimir Tsonev <krasimir@outset.ws>
2013-05-29 10:49:01 +0300

    Fixing commit message

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 # How to manage the profile file -> http://technet.microsoft.com/en-us/library/ee176913.aspx
-# Create 'WindowsPowerShell' in C:\Users\<username>\Documents\ and put the file there
+# Create 'WindowsPowerShell' in C:\Users\<username>\Documents\ and put the file there 
 
 function goToProjects {
 	set-location d:\work
