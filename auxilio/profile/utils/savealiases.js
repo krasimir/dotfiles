d4861dd82b73e8988cc2d5b2fbecfc0d7a0e6ac7
Krasimir Tsonev <krasimir@outset.ws>
2013-06-30 14:49:28 +0300

    Removing save aliases

--- auxilio/profile/utils/savealiases.js
+++
@@ -1,3 +0,0 @@
-function savealiases(args) {
-	exec('block start && alias exportallplease && writefile D:/work/KrasimirTsonev/private/dotfiles/auxilio/aliases.aux && cd D:/work/KrasimirTsonev/private/dotfiles && ci "Updating aliases" && push master && block end');
-}
