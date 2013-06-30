66a69dae9b4926fa44f25c96b3687b10ebf4d0bc
Krasimir Tsonev <krasimir@outset.ws>
2013-06-30 15:03:42 +0300

    Updating dotfiles

--- auxilio/profile/os/gitshell.js
+++ auxilio/profile/os/gitshell.js
@@ -1,3 +1,3 @@
 function gitshel(args) {
-	exec("C:\Windows\System32\cmd.exe /C  C:\Users\krasimir\AppData\Local\GitHub\GitHub.appref-ms --open-shell");
+	exec("C:\\Windows\\System32\\cmd.exe /C C:\\Users\\krasimir\\AppData\\Local\\GitHub\\GitHub.appref-ms --open-shell");
 }
