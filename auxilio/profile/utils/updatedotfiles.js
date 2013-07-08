b2e247f07da1dfb14c34f7d8fe35a85de851a935
Krasimir Tsonev <krasimir@outset.ws>
2013-07-08 11:32:42 +0300

    Updating dotfiles

--- auxilio/profile/utils/updatedotfiles.js
+++ auxilio/profile/utils/updatedotfiles.js
@@ -1,20 +1,19 @@
 function updateAuxilioManuals(args, callback) {
-	block("start", function() {
-		exec("cd D:/work/KrasimirTsonev/github/dotfiles", function() {
-			ci("Updating dotfiles", function() {
-				push("master", function() {
-					block("end", callback);
-				})					
-			})
-		});
-	})
-
-	/*chain([
-		["block", "start"],
-		["cd", "D:/work/KrasimirTsonev/github/dotfiles"],
-		["ci", "Updating dotfiles"],
-		["push", "master"],
-		["block", "end"]
-	], callback)*/
-	
+	Chain.on("done", callback).run(
+		function(res, next) {
+			block("start", next);
+		},
+		function(res, next) {
+			exec("cd D:/work/KrasimirTsonev/github/dotfiles", next);
+		},
+		function(res, next) {
+			ci("Updating dotfiles", next);
+		},
+		function(res, next) {
+			push("master", next);
+		},
+		function(res, next) {
+			block("end", next);
+		}
+	)
 }
