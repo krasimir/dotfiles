d9135388bc3d5898c61a0e5af67eaea0437121ff
Krasimir Tsonev <krasimir@outset.ws>
2013-07-08 16:56:05 +0300

    Updating dotfiles

--- auxilio/profile/github/push.js
+++ auxilio/profile/github/push.js
@@ -1,6 +1,7 @@
 function gitpush(args, callback) {
 	var branch = args.length > 0 ? args.shift() : false;
 	var remote = args.length > 0 ? args.shift() : 'origin';
+	if(typeof remote == 'object') remote = 'origin';
 	if(!branch) {
 		exec('error Missing branch.', callback);
 	} else {
