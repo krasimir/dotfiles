58917574455700afde08c5cd9f0de9fb0cbd8db3
Krasimir Tsonev <krasimir@outset.ws>
2013-07-19 10:31:10 +0300

    Updating dotfiles

--- auxilio/profile/github/log.js
+++ auxilio/profile/github/log.js
@@ -1,3 +1,4 @@
 function gitlog(args, callback) {
-	exec('git log -15 --format="%Cgreen%cn / %s%n%ai / %h"', callback);
+	var numberOfCommits = args.length > 0 ? parseInt(args[0]) : 10;
+	shell('git log -' + numberOfCommits + ' --name-status --format="%n------------------------------------ %s%n%cn%n%ai / %h"', callback);
 }
